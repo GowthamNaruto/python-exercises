ee7f585415779064fb136218a4319cc2993e5443
GowthamNaruto <gowthamgoutam@gmail.com>
2023-07-30 15:52:55 +0530

    Added hugging face transformers

---
+++ GPT/transformers.py
@@ -0,0 +1,44 @@
+import tensorflow as tf
+import os
+import math
+import numpy as np
+
+from tensorflow import keras
+from tensorflow.keras import layers
+from tensorflow.keras.utils import load_img
+from tensorflow.keras.utils import array_to_img
+from tensorflow.keras.utils import img_to_array
+from tensorflow.keras.preprocessing import image_dataset_from_directory
+
+from IPython.display import display
+
+# Load data: BSDS500 dataset
+dataset_url = "http://www.eecs.berkeley.edu/Research/Projects/CS/vision/grouping/BSR/BSR_bsds500.tgz"
+data_dir = keras.utils.get_file(origin=dataset_url, fname="BSR", untar=True)
+root_dir = os.path.join(data_dir, "BSDS500/data")
+
+# Create training and validate datasets
+crop_size = 3000
+upscale_factor = 3
+input_size = crop_size // upscale_factor
+batch_size = 8
+
+train_ds = image_dataset_from_directory(
+    root_dir,
+    batch_size=batch_size,
+    image_size=(crop_size, crop_size),
+    validation_split=0.2,
+    subset="training",
+    seed=1337,
+    label_mode=None,
+)
+
+valid_ds = image_dataset_from_directory(
+    root_dir,
+    batch_size=batch_size,
+    image_size=(crop_size, crop_size),
+    validation_split=0.2,
+    subset="validation",
+    seed=1337,
+    label_mode=None,
+)
